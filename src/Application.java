53c16b75246a4fb7aa4f273d49e0a61325fbd1d6
tgidk <tgidk>
2015-10-07 21:33:54 +0200

    first commit

---
+++ src/Application.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Application {
+
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<>();
+		list.add("dog");
+		list.add("cat");
+
+		String animal = list.get(0);
+
+	}
+
+}
